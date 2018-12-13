76ab069bbc199aaf29288918abe01b4be8dda3a5
wang-bin <wbsecg1@gmail.com>
2018-12-13 17:29:34 +0800

    add convenience macros, types & functions for ms

---
+++ MSUtils.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2018 WangBin <wbsecg1 at gmail.com>
+ */
+#include "MSUtils.h"
+
+// xxxxxxxx-0000-0010-8000-00AA00389B71
+uint32_t to_fourcc(const GUID id)
+{
+    if (id.Data2 == 0 && id.Data3 == 0x0010
+        && id.Data4[0] == 0x80 && id.Data4[1] == 0x00
+        && id.Data4[2] == 0x00 && id.Data4[3] == 0xAA && id.Data4[4] == 0x00
+        && id.Data4[5] == 0x38 && id.Data4[6] == 0x9B && id.Data4[7] == 0x71)
+        return id.Data1;
+    return 0;
+}
---
+++ MSUtils.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 WangBin <wbsecg1 at gmail.com>
+ */
+#pragma once
+#include <memory>
+#include <iostream>
+#include <string>
+#include <system_error>
+#include <windows.h>
+#ifdef WINAPI_FAMILY
+# include <winapifamily.h>
+# if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#   define MS_API_DESKTOP 1
+# else
+#   define MS_WINRT 1
+#   define MS_API_APP 1
+# endif
+#else
+# define MS_API_DESKTOP 1
+#endif //WINAPI_FAMILY
+
+// vista is required to build with wrl, but xp runtime works
+#pragma push_macro("_WIN32_WINNT")
+#if _WIN32_WINNT < _WIN32_WINNT_VISTA
+# undef _WIN32_WINNT
+# define _WIN32_WINNT _WIN32_WINNT_VISTA
+#endif
+#include <wrl/client.h>
+#if (_MSC_VER + 0) // missing headers in mingw
+#include <wrl/implements.h> // RuntimeClass
+#endif
+#pragma pop_macro("_WIN32_WINNT")
+using namespace Microsoft::WRL; //ComPtr
+
+// TODO: define MS_ERROR_STR before including this header to handle module specific errors?
+#define MS_ENSURE(f, ...) MS_CHECK(f, return __VA_ARGS__;)
+#define MS_WARN(f) MS_CHECK(f)
+#define MS_CHECK(f, ...)  do { \
+        while (FAILED(GetLastError())) {} \
+        HRESULT _hr_ = f; \
+        if (FAILED(_hr_)) { \
+            std::clog << #f "  ERROR@" << __LINE__ << __FUNCTION__ << ": (" << std::hex << _hr_ << std::dec << ") " << std::error_code(_hr_, std::system_category()).message() << std::endl; \
+            __VA_ARGS__ \
+        } \
+    } while (false)
+
+#define DX_ENSURE(f, ...) MS_CHECK(f, return __VA_ARGS__;)
+#define DX_WARN(f) MS_CHECK(f)
+
+
+using module_t = std::remove_pointer<HMODULE>::type;
+using dll_t = std::unique_ptr<module_t, decltype(&FreeLibrary)>;
+
+uint32_t to_fourcc(const GUID id);
