3e263546d44118938ee03b8892b3d60ae81054ca
wang-bin <wbsecg1@gmail.com>
2020-10-13 09:46:43 +0800

    d3d11: levels, adapter, win7 & debug support

--- D3D11Utils.cpp
+++ D3D11Utils.cpp
@@ -1,17 +1,89 @@
 /*
- * Copyright (c) 2018-2019 WangBin <wbsecg1 at gmail.com>
- * This file is part of MDK MFT plugin
- * Source code: https://github.com/wang-bin/mdk-mft
- * 
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2018-2020 WangBin <wbsecg1 at gmail.com>
  */
 #include "D3D11Utils.h"
 #include <iostream>
+#include <type_traits>
+#include <d3d11_1.h>
+#include "base/fmt.h"
 // gpu select: https://github.com/walbourn/directx-vs-templates/commit/e6406ee9afaf2719ff29b68c63c7adbcac57c02a
 
 namespace D3D11 {
+using namespace std;
+D3D_FEATURE_LEVEL to_feature_level(float value)
+{
+    if (value < 9.1f)
+        return D3D_FEATURE_LEVEL_12_1;
+    if (value >= 12.1f)
+        return D3D_FEATURE_LEVEL_12_1;
+    else if (value >= 12.0f)
+        return D3D_FEATURE_LEVEL_12_0;
+    else if (value >= 11.1f)
+        return D3D_FEATURE_LEVEL_11_1;
+    else if (value >= 11.0f)
+        return D3D_FEATURE_LEVEL_11_0;
+    else if (value >= 10.1f)
+        return D3D_FEATURE_LEVEL_10_1;
+    else if (value >= 10.0f)
+        return D3D_FEATURE_LEVEL_10_0;
+    else if (value >= 9.3f)
+        return D3D_FEATURE_LEVEL_9_3;
+    else if (value >= 9.2f)
+        return D3D_FEATURE_LEVEL_9_2;
+    else if (value >= 9.1f)
+        return D3D_FEATURE_LEVEL_9_1;
+    return D3D_FEATURE_LEVEL_11_1;
+}
+
+D3D_FEATURE_LEVEL to_feature_level(const char* name)
+{
+    float fl = 12.1f;
+    if (name)
+        fl =  std::stof(name);
+    return to_feature_level(fl);
+}
+
+static void debug_to_log(ID3D11InfoQueue* iq)
+{
+    auto nb_msgs = iq->GetNumStoredMessages();
+    for (UINT64 i = 0; i < nb_msgs; ++i) {
+        SIZE_T len = 0;
+        MS_ENSURE(iq->GetMessage(0, NULL, &len));
+        D3D11_MESSAGE* msg = (D3D11_MESSAGE*)malloc(len);
+        if (FAILED(iq->GetMessage(0, msg, &len))) {
+            free(msg);
+            return;
+        }
+        clog << fmt::to_string("ID %d(Severity %d): %.*s", msg->ID, msg->Severity, (int)msg->DescriptionByteLength, msg->pDescription) << endl;
+        free(msg);
+    }
+    iq->ClearStoredMessages();
+}
+
+void debug_report(ID3D11Device* dev)
+{
+    if (!dev)
+        return;
+     ComPtr<ID3D11Debug> dbg;
+     if (FAILED(dev->QueryInterface(IID_PPV_ARGS(&dbg))))
+        return;
+    ComPtr<ID3D11InfoQueue> iq;
+     if (FAILED(dev->QueryInterface(IID_PPV_ARGS(&iq))))
+        return;
+    // Using ID3D11Debug::ReportLiveDeviceObjects with D3D11_RLDO_DETAIL will help drill into object lifetimes. Objects with Refcount=0 and IntRef=0 will be eventually destroyed through typical Immediate Context usage. However, if the application requires these objects to be destroyed sooner, ClearState followed by Flush on the Immediate Context will realize their destruction.
+    {
+        ComPtr<ID3D11DeviceContext> ctx;
+        dev->GetImmediateContext(&ctx);
+        ctx->ClearState();
+        ctx->Flush();
+    }
+    clog << fmt::to_string("------------debug report begin. device %p------------", dev) << endl;
+    dbg->ReportLiveDeviceObjects(D3D11_RLDO_DETAIL); //D3D11_RLDO_IGNORE_INTERNAL
+    debug_to_log(iq.Get());
+    dbg->ReportLiveDeviceObjects(D3D11_RLDO_SUMMARY);
+    debug_to_log(iq.Get());
+    clog << fmt::to_string("------------debug report end. device %p------------", dev) << endl;
+}
 
 ComPtr<IDXGIFactory> CreateDXGI() // TODO: int version = 2. 1.0/1.1 can not be mixed
 {
@@ -39,57 +111,86 @@ ComPtr<IDXGIFactory> CreateDXGI() // TODO: int version = 2. 1.0/1.1 can not be m
 }
 
 // Starting with Windows 8, an adapter called the "Microsoft Basic Render Driver" is always present. This adapter has a VendorId of 0x1414 and a DeviceID of 0x8c
-ComPtr<ID3D11Device> CreateDevice(ComPtr<IDXGIFactory> dxgi, UINT adapterIndex)
+ComPtr<ID3D11Device> CreateDevice(ComPtr<IDXGIFactory> dxgi, int adapterIndex, D3D_FEATURE_LEVEL fl, UINT flags)
 {
-    if (!dxgi)
-        return nullptr;
-#if (MS_API_DESKTOP+0)
-    HMODULE d3d11_dll = GetModuleHandleA("d3d11.dll");
-    if (!d3d11_dll)
-        return nullptr;
-    auto D3D11CreateDevice = (PFN_D3D11_CREATE_DEVICE)GetProcAddress(d3d11_dll, "D3D11CreateDevice");
-    if (!D3D11CreateDevice)
-        return nullptr;
-#endif
+    // always link to d3d11 to ensure d3d11.dll is loaded even if Manager is not used
     ComPtr<IDXGIAdapter> adapter;
-    MS_WARN(dxgi->EnumAdapters(adapterIndex, &adapter)); // dxgi2?
-    DXGI_ADAPTER_DESC desc;
-    MS_ENSURE(adapter->GetDesc(&desc), nullptr);
-    char description[256]{};
-    snprintf(description, sizeof(description), "%ls", desc.Description);
-    std::clog << "adapter " << adapterIndex << ": " << description << std::endl;
-    const D3D_FEATURE_LEVEL* fls = nullptr;
+    if (dxgi && adapterIndex >= 0) {
+        MS_WARN(dxgi->EnumAdapters(adapterIndex, &adapter)); // dxgi2?
+        DXGI_ADAPTER_DESC desc;
+        MS_ENSURE(adapter->GetDesc(&desc), nullptr);
+        char description[256]{};
+        snprintf(description, sizeof(description), "%ls", desc.Description);
+        clog << fmt::to_string("d3d11 adapter %d: %s", adapterIndex, description) << endl;
+    } else {
+        std::clog << "d3d11 use default adapter" << std::endl;
+    }
+    const D3D_FEATURE_LEVEL fls[] = {
+        fl,
+        D3D_FEATURE_LEVEL_12_1,
+        D3D_FEATURE_LEVEL_12_0,
+        D3D_FEATURE_LEVEL_11_1,
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0,
+        D3D_FEATURE_LEVEL_9_3, // win7, win10/8 mobile/phone
+        D3D_FEATURE_LEVEL_9_2,
+        D3D_FEATURE_LEVEL_9_1, // low power devices
+    };
     ComPtr<ID3D11Device> dev;
-    // adapter non-null, type D3D_DRIVER_TYPE_HARDWARE: E_INVALIDARG 
-    MS_ENSURE(D3D11CreateDevice(adapter.Get(), adapter ? D3D_DRIVER_TYPE_UNKNOWN : D3D_DRIVER_TYPE_HARDWARE
-        , nullptr, D3D11_CREATE_DEVICE_VIDEO_SUPPORT, fls, 0
+    // d3d11.1: MUST explicitly provide an array includes D3D_FEATURE_LEVEL_11_1. If Direct3D 11.1 runtime is not available, immediately fails with E_INVALIDARG.
+    HRESULT hr = S_OK;
+    size_t fl_index = 0;
+    for (int i = 1; i < std::extent<decltype(fls)>::value; ++i) {
+        if (fls[i] == fl) {
+            fl_index = i;
+            break;
+        }
+    }
+    // adapter non-null, type D3D_DRIVER_TYPE_HARDWARE: E_INVALIDARG
+    MS_WARN((hr = D3D11CreateDevice(adapter.Get(), adapter ? D3D_DRIVER_TYPE_UNKNOWN : D3D_DRIVER_TYPE_HARDWARE
+        , nullptr, flags, &fls[fl_index], (UINT)(std::extent<decltype(fls)>::value - fl_index)
+        , D3D11_SDK_VERSION, &dev, nullptr, nullptr)));
+    if (hr == E_INVALIDARG) // if fls contains 11_1 but not supported by runtime. null fls will try 11_0~9_1
+        MS_ENSURE(D3D11CreateDevice(adapter.Get(), adapter ? D3D_DRIVER_TYPE_UNKNOWN : D3D_DRIVER_TYPE_HARDWARE
+        , nullptr, flags, nullptr, 0 //&fls[2], std::extent<decltype(fls)>::value - 2
         , D3D11_SDK_VERSION, &dev, nullptr, nullptr), nullptr);
+    ComPtr<ID3D11Device1> dev1;
+    MS_WARN(dev.As(&dev1));
+    clog << fmt::to_string("d3d11.%d device feature level: %#x, requested: %#x.", dev1.Get() ? 1 : 0, dev->GetFeatureLevel(), fl) << endl;
+    // if null adapter, print adapter info
     ComPtr<ID3D10Multithread> mt;
     if (SUCCEEDED(dev.As(&mt)))
         mt->SetMultithreadProtected(TRUE);
     return dev;
 }
 
-
-bool Manager::init(UINT adapterIndex)
+ComPtr<ID3D11Device> CreateDevice(int adapterIndex, D3D_FEATURE_LEVEL fl, UINT flags)
 {
-    if (!dxgi_) {
 #if (MS_API_DESKTOP+0)
-        dxgi_dll_.reset(LoadLibraryA("dxgi.dll"));
-        if (!dxgi_dll_)
-            return false;
+    dll_t dxgi_dll{nullptr, &FreeLibrary};
+    dxgi_dll.reset(LoadLibraryA("dxgi.dll"));
+    if (!dxgi_dll)
+        return nullptr;
 #endif
-        dxgi_ = D3D11::CreateDXGI();
-    }
-    if (!dxgi_)
-        return false;
+    auto dxgi = D3D11::CreateDXGI();
+    return CreateDevice(dxgi.Get(), adapterIndex, fl, flags);
+}
+
+bool Manager::init(int adapterIndex, D3D_FEATURE_LEVEL fl, UINT flags)
+{
 #if (MS_API_DESKTOP+0)
     d3d11_dll_.reset(LoadLibraryA("d3d11.dll"));
 #endif
-    dev_ = D3D11::CreateDevice(dxgi_, adapterIndex);
+    dev_ = D3D11::CreateDevice(adapterIndex, fl, D3D11_CREATE_DEVICE_VIDEO_SUPPORT|flags);
     if (!dev_)
         return false;
     return true;
+}
+
+Manager::~Manager()
+{
+    debug_report(dev_.Get());
 }
 
 ComPtr<IMFDXGIDeviceManager> Manager::create()
@@ -104,8 +205,18 @@ ComPtr<IMFDXGIDeviceManager> Manager::create()
     typedef HRESULT (STDAPICALLTYPE *MFCreateDXGIDeviceManager_pfn)(UINT*, IMFDXGIDeviceManager**);
     auto MFCreateDXGIDeviceManager = (MFCreateDXGIDeviceManager_pfn)GetProcAddress(mfplat_dll_.get(), "MFCreateDXGIDeviceManager");
     if (!MFCreateDXGIDeviceManager) {
-        std::clog << "MFCreateDXGIDeviceManager is not found in mfplat.dll" << std::endl;
-        return nullptr;
+        std::clog << "MFCreateDXGIDeviceManager is not found in mfplat.dll. try mshtmlmedia.dll" << std::endl;
+        // https://chromium.googlesource.com/chromium/src.git/+/62.0.3178.1/media/gpu/dxva_video_decode_accelerator_win.cc?autodive=0%2F%2F%2F%2F
+        mfplat_dll_.reset(LoadLibraryA("mshtmlmedia.dll"));
+        if (!mfplat_dll_) {
+            std::clog << "Failed to load mshtmlmedia.dll" << std::endl;
+            return nullptr;
+        }
+        MFCreateDXGIDeviceManager = (MFCreateDXGIDeviceManager_pfn)GetProcAddress(mfplat_dll_.get(), "MFCreateDXGIDeviceManager");
+        if (!MFCreateDXGIDeviceManager) {
+            std::clog << "MFCreateDXGIDeviceManager is not found" << std::endl;
+            return nullptr;
+        }
     }
 #endif
     MS_ENSURE(MFCreateDXGIDeviceManager(&token, &mgr_), nullptr);
--- D3D11Utils.h
+++ D3D11Utils.h
@@ -1,11 +1,5 @@
 /*
- * Copyright (c) 2018 WangBin <wbsecg1 at gmail.com>
- * This file is part of MDK MFT plugin
- * Source code: https://github.com/wang-bin/mdk-mft
- * 
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2018-2020 WangBin <wbsecg1 at gmail.com>
  */
 #pragma once
 # pragma push_macro("_WIN32_WINNT")
@@ -16,25 +10,30 @@
 #include "base/ms/MSUtils.h"
 #include <d3d11.h>
 #include <dxgi1_2.h>
-#include <mfapi.h> // MFCreateDXGIDeviceManager 
+#include <mfapi.h> // MFCreateDXGIDeviceManager
 #include <mfidl.h>
 # pragma pop_macro("_WIN32_WINNT")
 
 namespace D3D11 {
 
 ComPtr<IDXGIFactory> CreateDXGI();
-ComPtr<ID3D11Device> CreateDevice(ComPtr<IDXGIFactory> dxgi, UINT adapterIndex = 0);
+// result feature level will be <= requested value
+ComPtr<ID3D11Device> CreateDevice(ComPtr<IDXGIFactory> dxgi, int adapterIndex = 0, D3D_FEATURE_LEVEL = D3D_FEATURE_LEVEL_12_1, UINT flags = 0);
+ComPtr<ID3D11Device> CreateDevice(int adapterIndex = 0, D3D_FEATURE_LEVEL = D3D_FEATURE_LEVEL_12_1, UINT flags = 0);
+D3D_FEATURE_LEVEL to_feature_level(float value = 0);
+D3D_FEATURE_LEVEL to_feature_level(const char* name = nullptr);
+void debug_report(ID3D11Device* dev);
 
 class Manager
 {
 public:
-    bool init(UINT adapterIndex = 0);
+    ~Manager();
+// adapterIndex: -1 default
+    bool init(int adapterIndex = 0, D3D_FEATURE_LEVEL = D3D_FEATURE_LEVEL_12_1, UINT flags = 0);
     ComPtr<IMFDXGIDeviceManager> create(); // win8+
 private:
     dll_t d3d11_dll_{nullptr, &FreeLibrary};
-    dll_t dxgi_dll_{nullptr, &FreeLibrary};
     dll_t mfplat_dll_{nullptr, &FreeLibrary};
-    ComPtr<IDXGIFactory> dxgi_; // create first, destroy last
     ComPtr<ID3D11Device> dev_;
     ComPtr<IMFDXGIDeviceManager> mgr_;
 };
